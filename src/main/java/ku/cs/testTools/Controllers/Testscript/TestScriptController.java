8a0fe1bbbf8ca4ad7be9b98ebf1a9b4c10585a9c
1Miquella <suneepond.p@kuth>
2024-09-01 17:13:33 +0700

    add path controller

--- src/main/java/ku/cs/testTools/Controllers/Testscript/TestScriptController.java
+++ src/main/java/ku/cs/testTools/Controllers/Testscript/TestScriptController.java
@@ -1,4 +1,49 @@
 package ku.cs.testTools.Controllers.Testscript;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
 public class TestScriptController {
+
+    @FXML
+    private Hyperlink onClickTestcase;
+
+    @FXML
+    private Hyperlink onClickTestflow;
+
+    @FXML
+    private Hyperlink onClickTestresult;
+
+    @FXML
+    private Hyperlink onClickTestscript;
+
+    @FXML
+    private Hyperlink onClickUsecase;
+
+    @FXML
+    void onClickTestcase(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickTestflow(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickTestresult(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickTestscript(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickUsecase(ActionEvent event) {
+
+    }
+
 }
