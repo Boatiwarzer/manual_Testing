e33b3b5f821251adc6a61f51a9b71d9539566564
1Miquella <suneepond.p@kuth>
2024-09-07 17:24:28 +0700

    Add TestResult

---
+++ src/main/java/ku/cs/testTools/Controllers/testResult/PopupAddTestresultController.java
@@ -0,0 +1,85 @@
+package ku.cs.testTools.Controllers.testResult;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class PopupAddTestresultController {
+
+    @FXML
+    private TextField onActual;
+
+    @FXML
+    private Button onCancelButton;
+
+    @FXML
+    private Button onConfirmButton;
+
+    @FXML
+    private Label onDate;
+
+    @FXML
+    private Label onDescription;
+
+    @FXML
+    private Label onExpected;
+
+    @FXML
+    private TextField onRole;
+
+    @FXML
+    private TextField onStatus;
+
+    @FXML
+    private TableView<?> onTableTeststeps;
+
+    @FXML
+    private TextField onTestNo;
+
+    @FXML
+    private Label onTester;
+
+    @FXML
+    private ComboBox<?> onTestscriptIDComboBox;
+
+    @FXML
+    void onActual(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCancelButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onConfirmButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRole(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onStatus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onTestNo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onTestscriptIDComboBox(ActionEvent event) {
+
+    }
+
+}
+
---
+++ src/main/java/ku/cs/testTools/Controllers/testResult/TestResultEditController.java
@@ -0,0 +1,126 @@
+package ku.cs.testTools.Controllers.testResult;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class TestResultEditController {
+
+    @FXML
+    private Button onAddButton;
+
+    @FXML
+    private Hyperlink onClickTestcase;
+
+    @FXML
+    private Hyperlink onClickTestflow;
+
+    @FXML
+    private Hyperlink onClickTestresult;
+
+    @FXML
+    private Hyperlink onClickTestscript;
+
+    @FXML
+    private Hyperlink onClickUsecase;
+
+    @FXML
+    private Button onDeleteButton;
+
+    @FXML
+    private Button onDeleteListButton;
+
+    @FXML
+    private Button onSearchButton;
+
+    @FXML
+    private TextField onSearchField;
+
+    @FXML
+    private ListView<?> onSearchList;
+
+    @FXML
+    private Button onSubmitButton;
+
+    @FXML
+    private TableView<?> onTableTestresult;
+
+    @FXML
+    private TextField onTestNameField;
+
+    @FXML
+    private TextField onTestNoteField;
+
+    @FXML
+    private Label testDateLabel;
+
+    @FXML
+    private Label testIDLabel;
+
+    @FXML
+    void onAddButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickTestcase(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickTestflow(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickTestresult(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickTestscript(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickUsecase(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteListButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSearchButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSubmitButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onTestNameField(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onTestNoteField(ActionEvent event) {
+
+    }
+
+}
+
