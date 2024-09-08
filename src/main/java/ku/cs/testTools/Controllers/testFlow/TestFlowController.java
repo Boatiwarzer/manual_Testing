fb63bab5c1edfc413bfb0b69073303436098af75
1Miquella <suneepond.p@kuth>
2024-09-08 17:27:47 +0700

    Add TestFlow

---
+++ src/main/java/ku/cs/testTools/Controllers/testFlow/TestFlowController.java
@@ -0,0 +1,70 @@
+package ku.cs.testTools.Controllers.testFlow;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.AnchorPane;
+
+public class TestFlowController {
+
+    @FXML
+    private TitledPane componentTitlePane;
+
+    @FXML
+    private TitledPane disignTitlePane;
+
+    @FXML
+    private TitledPane noteTitlePane;
+
+    @FXML
+    private Hyperlink onClickTestcase;
+
+    @FXML
+    private Hyperlink onClickTestflow;
+
+    @FXML
+    private Hyperlink onClickTestresult;
+
+    @FXML
+    private Hyperlink onClickTestscript;
+
+    @FXML
+    private Hyperlink onClickUsecase;
+
+    @FXML
+    private AnchorPane onComponentArea;
+
+    @FXML
+    private AnchorPane onDesignArea;
+
+    @FXML
+    private TextArea onNoteTextArea;
+
+    @FXML
+    void onClickTestcase(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickTestflow(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickTestresult(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickTestscript(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickUsecase(ActionEvent event) {
+
+    }
+
+}
