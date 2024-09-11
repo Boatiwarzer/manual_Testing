68415ccd8d54ff1c3331f48cc51eaf5af53b57c6
Puttipong <puttipong.srina@gmail.com>
2024-09-11 13:19:35 +0700

    insert NewprojectPage

--- src/main/java/ku/cs/testTools/Controllers/home/LandingController.java
+++ src/main/java/ku/cs/testTools/Controllers/home/LandingController.java
@@ -27,55 +27,56 @@ public class LandingController {
 
     @FXML
     void onOpenfileButton(ActionEvent event) throws IOException{
-    //     / Open file chooser
-    //     FileChooser fileChooser = new FileChooser();
-    //     fileChooser.setTitle("Open Project");
+        / Open file chooser
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Project");
 
-    //     // Set extension filter
-    //     FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TestTools files (*.csv)", "*.csv");
-    //     fileChooser.getExtensionFilters().add(extFilter);
+        // Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TestTools files (*.csv)", "*.csv");
+        fileChooser.getExtensionFilters().add(extFilter);
 
-    //     // Show open file dialog
-    //     File file = fileChooser.showOpenDialog(null);
-    //     if (file != null) {
-    //         System.out.println("Opening file: " + file.getName());
+        // Show open file dialog
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+            System.out.println("Opening file: " + file.getName());
 
-    //         // Get the project name from the file name
-    //         String projectName = file.getName().substring(0, file.getName().lastIndexOf("."));
+            // Get the project name from the file name
+            String projectName = file.getName().substring(0, file.getName().lastIndexOf("."));
 
-    //         // Get the directory from the file path
-    //         String directory = file.getParent();
+            // Get the directory from the file path
+            String directory = file.getParent();
 
-    //         // load preferenceList
-    //         PreferenceList preferenceList = new PreferenceList();
-    //         DataSource<PreferenceList> preferenceListDataSource = new PreferenceListFileDataSource(directory, projectName + ".csv");
-    //         preferenceList = preferenceListDataSource.readData(); // Read the PreferenceList from the CSV file
-    //         preferenceList.getPreferenceList().forEach(preference -> {
-    //             String theme  = preference.getTheme();
-    //             if (Objects.equals(theme, "Light")) {
-    //                 FXRouter.setTheme(1);
-    //                 System.out.println("light");
-    //             } else if (Objects.equals(theme, "Dark")) {
-    //                 FXRouter.setTheme(2);
-    //                 System.out.println("dark");
-    //             }
-    //         });
+            // load preferenceList
+            // PreferenceList preferenceList = new PreferenceList();
+            // DataSource<PreferenceList> preferenceListDataSource = new PreferenceListFileDataSource(directory, projectName + ".csv");
+            // preferenceList = preferenceListDataSource.readData(); // Read the PreferenceList from the CSV file
+            // preferenceList.getPreferenceList().forEach(preference -> {
+            //     String theme  = preference.getTheme();
+            //     if (Objects.equals(theme, "Light")) {
+            //         FXRouter.setTheme(1);
+            //         System.out.println("light");
+            //     } else if (Objects.equals(theme, "Dark")) {
+            //         FXRouter.setTheme(2);
+            //         System.out.println("dark");
+            //     }
+            // });
 
-    //         //send the project name and directory to HomePage
-    //         ArrayList<Object> objects = new ArrayList<>();
-    //         objects.add(projectName);
-    //         objects.add(directory);
-    //         FXRouter.when("HomePage", "ku/cs/usecasedesigner/home-page.fxml", "KU CS UseCaseDesigner | " + projectName);
-    //         FXRouter.goTo("HomePage", objects);
+            //send the project name and directory to HomePage
+            ArrayList<Object> objects = new ArrayList<>();
+            objects.add(projectName);
+            objects.add(directory);
+            // แก้พาท
+            String packageStr1 = "views/";
+            FXRouter.when("home", packageStr1 + "home.fxml","home", "TestTools | " + projectName);
+            FXRouter.goTo("home", objects);
+            // Close the current window
+            Node source = (Node) actionEvent.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            stage.close();
 
-    //         // Close the current window
-    //         Node source = (Node) actionEvent.getSource();
-    //         Stage stage = (Stage) source.getScene().getWindow();
-    //         stage.close();
-
-    //     } else {
-    //         System.out.println("No file selected.");
-    //     }
+        } else {
+            System.out.println("No file selected.");
+        }
 
      }
 }
--- src/main/java/ku/cs/testTools/Controllers/home/NewProjectController.java
+++ src/main/java/ku/cs/testTools/Controllers/home/NewProjectController.java
@@ -21,22 +21,71 @@ public class NewProjectController {
 
     @FXML
     void onCancelButton(ActionEvent event) {
+        Node source = (Node) actionEvent.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
 
     }
 
     @FXML
     void onConfirmButton(ActionEvent event) {
+        System.out.println("Confirm button clicked.");
 
-    }
+        // Check if the system name and directory are empty
+        if (SystemNameTextField.getText().isEmpty()) {
+            systemNameErrorText.setText("Please enter a name.");
+            return;
+        } else {
+            systemNameErrorText.setText("");
+        }
 
-    @FXML
-    void onProjectNameField(ActionEvent event) {
+        if (directory == null) {
+            directoryErrorText.setText("Please select a directory.");
+            return;
+        } else {
+            directoryErrorText.setText("");
+        }
 
-    }
+        // Set value for projectName
+        String projectName = SystemNameTextField.getText();
+
+        setWindowTitle(projectName);
+
+        //send the project name and directory to HomePage
+        ArrayList<Object> objects = new ArrayList<>();
+        objects.add(projectName);
+        objects.add(directory);
+        //แก้พาท
+        String packageStr1 = "views/";
+        FXRouter.when("home", packageStr1 + "home.fxml","home", "TestTools | " + projectName);
+        FXRouter.goTo("home", objects);
+
+        // Close the current window
+        Node source = (Node) actionEvent.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+
+        }
 
     @FXML
     void onSelectButton(ActionEvent event) {
+        System.out.println("Select button clicked.");
+        // Create directory chooser
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select Directory");
 
+        // Show directory chooser
+        File file = directoryChooser.showDialog(null);
+        if (file != null) {
+            System.out.println("Selected directory: " + file.getAbsolutePath());
+            directory = file.getAbsolutePath();
+            selectButton.setText(directory);
+            } else {
+            System.out.println("No directory selected.");
+            }
     }
-
+    private void setWindowTitle(String projectName) {
+        Stage stage = (Stage) SystemNameTextField.getScene().getWindow();
+        stage.setTitle(projectName);
+    }   
 }
