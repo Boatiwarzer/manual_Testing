b214062f7d4a94d84f6ac3c7fcd2fc695c9c6c58
Puttipong Srina <puttipong.srina@gmail.com>
2024-09-16 18:15:58 +0700

    fix home

--- src/main/java/ku/cs/testTools/Controllers/Home/HomeController.java
+++ src/main/java/ku/cs/testTools/Controllers/Home/HomeController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Hyperlink;
 import ku.cs.fxrouter.FXRouter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class HomeController {
 
@@ -23,6 +24,7 @@ public class HomeController {
 
     @FXML
     private Hyperlink onClickUsecase;
+    private String projectName, directory;
 
     @FXML
     void onClickTestcase(ActionEvent event) {
@@ -66,6 +68,20 @@ public class HomeController {
             FXRouter.goTo("use_case");
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+    @FXML
+    void initialize() {
+        if (FXRouter.getData() != null) {
+            ArrayList<Object> objects = (ArrayList) FXRouter.getData();
+            // Load the project
+            projectName = (String) objects.get(0);
+            directory = (String) objects.get(1);
+
+//            loadProject();
+//            saveProject();
+            System.out.println("Project Name: " + projectName);
+            System.out.println("Directory: " + directory);
         }
     }
 
